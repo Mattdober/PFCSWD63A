9ef5b4b983d162af9936c8aa1064c1d89d7dde82
Mattdober <46972346+Mattdober@users.noreply.github.com>
2021-03-04 17:18:31 +0100

    added file upload

--- WebApplication1/WebApplication1/Controllers/BlogsController.cs
+++ WebApplication1/WebApplication1/Controllers/BlogsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Cloud.Storage.V1;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +14,10 @@ namespace WebApplication1.Controllers
     public class BlogsController : Controller
     {
         private readonly IBlogsRepository _blogsRepo;
-        public BlogsController(IBlogsRepository blogsRepo)
+        private readonly IConfiguration _config;
+        public BlogsController(IBlogsRepository blogsRepo, IConfiguration config)
         {
+            _config = config;
             _blogsRepo = blogsRepo;
         }
 
@@ -29,7 +34,7 @@ namespace WebApplication1.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Blog b)
+        public IActionResult Create(IFormFile logo, Blog b)
         {
             if (_blogsRepo.GetBlogs().Where(x => x.Url == b.Url).Count() == 0)
             {
@@ -37,6 +42,25 @@ namespace WebApplication1.Controllers
             }
             else
                 TempData["Warning"] = "This blog already exists";
+
+            string bucketName = _config.GetSection("AppSettings").GetSection("LogosBucket").Value;
+
+            string uniqueFilename = Guid.NewGuid() + System.IO.Path.GetExtension(logo.FileName);
+
+            //1. upload picture to cloud storage
+            if(logo != null)
+            {
+                var storage = StorageClient.Create();
+                using (var myFile = logo.OpenReadStream())
+                {
+                    storage.UploadObject(bucketName, uniqueFilename, null, myFile);
+                }
+                //2. add a reference to the picture to the blog > b
+                b.Url = $"https://storage.googleapis.com/{bucketName}/{uniqueFilename}";
+
+                //3. save everything into the db
+                _blogsRepo.AddBlog(b);
+            }
             return RedirectToAction("Index");
         }
     }
